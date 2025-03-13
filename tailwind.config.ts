1530bd7586d6523e6c9b960710ff45cbf096676e
Antonio <a.rinaldi.dev@gmail.com>
2025-03-13 12:57:11 +0100

    mi sto rompendo il cazzo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,6 @@
 import type { Config } from "tailwindcss";
-<<<<<<< HEAD
-import daisyui from "daisyui"
-import typography from '@tailwindcss/typography';
-=======
 import daisy from "daisyui"
 
->>>>>>> develop
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -21,10 +16,6 @@ const config: Config = {
       },
     },
   },
-<<<<<<< HEAD
-  plugins: [typography,daisyui],
-=======
   plugins: [require('daisyui'),],
->>>>>>> develop
 };
 export default config;
