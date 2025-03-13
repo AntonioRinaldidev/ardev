67c8788bcc478cb53538cf5cf7fd6bf8e8c8fed5
Antonio <a.rinaldi.dev@gmail.com>
2025-03-13 11:02:33 +0100

    boh erano mesi che non lo aprivo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisy from "daisyui"
 
 const config: Config = {
   content: [
@@ -15,6 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui'),],
 };
 export default config;
