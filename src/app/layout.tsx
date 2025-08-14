68fbc8718d0584915956f5beacc3dbae8a70a2ad
Antonio <a.rinaldi.dev@gmail.com>
2025-08-15 00:26:20 +0200

    favicon and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,30 @@ export const metadata: Metadata = {
 	authors: [{ name: 'Antonio Rinaldi', url: 'https://antoniorinaldidev.com' }],
 	creator: 'Antonio Rinaldi',
 	metadataBase: new URL('https://antoniorinaldidev.com'),
+	icons: {
+		icon: [
+			{ url: '/favicon.ico', sizes: 'any' },
+			{ url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+			{ url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+		],
+		apple: [
+			{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+		],
+		other: [
+			{
+				url: '/android-chrome-192x192.png',
+				sizes: '192x192',
+				type: 'image/png',
+			},
+			{
+				url: '/android-chrome-512x512.png',
+				sizes: '512x512',
+				type: 'image/png',
+			},
+		],
+	},
+
+	manifest: '/site.webmanifest',
 
 	appleWebApp: {
 		capable: true,
