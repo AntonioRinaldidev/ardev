aecef46855402356fb0c0b350ff5bed496b4a7f3
Antonio Rinaldi <a.rinaldi.dev@gmail.com>
2025-12-04 12:16:05 +0100

    Chore: Removing Welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useAppSelector, useAppDispatch } from '@/store/hooks';
-import WelcomeHeroUnified from '@/components/welcomeHero';
+import Hero from '@/components/Hero';
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
@@ -18,7 +18,7 @@ export default function Home() {
 	return (
 		<>
 			<div className="hero-container-page">
-				<WelcomeHeroUnified />
+				<Hero />
 			</div>
 		</>
 	);
