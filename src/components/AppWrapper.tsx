2e8f332e2b6e2758863c2100d43358924c0e192d
Antonio rinaldi <>
2025-12-19 23:47:08 +0100

    Chore: removing welcome imports

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -3,20 +3,13 @@
 import { ReactNode, useEffect } from 'react';
 import ThemeInitializer from './Initializers/ThemeInitializer';
 
-import WelcomeInitializer from './Initializers/WelcomeInitializer';
 import { useAppDispatch } from '@/store/hooks';
-import { initializeWelcome } from '@/store/welcomeSlice';
 
 export default function AppWrapper({ children }: { children: ReactNode }) {
 	const dispatch = useAppDispatch();
 
-	useEffect(() => {
-		dispatch(initializeWelcome());
-	}, [dispatch]);
-
 	return (
 		<>
-			<WelcomeInitializer />
 			<ThemeInitializer />
 			{children}
 		</>
